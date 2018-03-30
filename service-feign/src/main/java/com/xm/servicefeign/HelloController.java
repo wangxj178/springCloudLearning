320a291dc12a8abaa76899b13032eb111524c21a
wangxijun <wangxj178@qq.com>
2018-03-30 15:48:43 +0800

    new project

---
+++ service-feign/src/main/java/com/xm/servicefeign/HelloController.java
@@ -0,0 +1,20 @@
+package com.xm.servicefeign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String hi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+
+}
